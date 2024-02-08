d57ec0acd9856e6445826cf7f9f3594fe1b44967
virtouso <s_moeen_kamali@yahoo.com>
2024-02-08 12:19:24 +0330

    added whole framework

---
+++ Mc2.CrudTest.Presentation.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Mc2.CrudTest.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
