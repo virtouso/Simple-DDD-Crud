93438df6b5f3f0d63d4d2a56752f4ecd163b6b09
virtouso <s_moeen_kamali@yahoo.com>
2024-02-08 14:36:51 +0330

    added basic entity for customer

---
+++ Mc2.CrudTest.Domain.UnitTests/CustomerTests.cs
@@ -0,0 +1,18 @@
+﻿using Mc2.CrudTest.Domain.Entities;
+
+namespace Mc2.CrudTest.Domain.UnitTests;
+
+public class CustomerTests
+{
+    [Fact]
+    public void CreateCustomer_ReturnsValidObject()
+    {
+        //arrange
+        //act
+        Customer customer = new Customer();
+
+        //assert
+        Assert.NotNull(customer);
+        
+    }
+}
